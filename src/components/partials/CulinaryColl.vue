841ab2ec82e00f34171a3bd9a5faf50aa2316685
manueledigregorio <dig.manuele@gmail.com>
2023-10-21 12:48:15 +0200

    [ADD] Culinary

---
+++ src/components/partials/CulinaryColl.vue
@@ -0,0 +1,83 @@
+<script>
+import culinary from '../../data/partials/culinary';
+export default {
+name:'CulinaryColl',
+data() {
+  return {
+    culinary
+  }
+},
+}
+</script>
+
+<template>
+  <div class="container">
+    <div class="d-flex justify-content-center">
+      <div class="text">
+        <h4>CULINARY COLLECTION</h4>
+        <p>
+          Aliquam erat volutpat.Ut quis Lorem, ipsum reiciendis iusto,
+          blanditiis hic deserunt accusantium ullam mollitia delectus.
+        </p>
+      </div>
+    </div>
+
+    <div class="row">
+      <div 
+      v-for="(items, index) in culinary"
+      :key="index"
+       class="col-3 mb-4 ">
+        <div class="card-custom">
+          <img :src="items.img" alt="">
+          <div class=" px-2 w-100 ">
+            <a href="">{{ items.text }}</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+  </div>
+  
+</template>
+
+
+<style lang="scss" scoped>
+
+@use '../../sass/partials/variabiles.scss' as *;
+ 
+.text {
+  text-align: center;
+  width: 40%;
+  h4{
+  padding: 20px;
+  }
+
+  p {
+    padding-bottom: 30px;
+  }
+}
+
+.card-custom{
+  width: 100%;
+  background-color: $white;
+  text-align: center;
+  padding-bottom: 10px;
+
+  img{
+    width: 100px;
+  }
+
+  a{
+    color: $black;
+    display: block;
+    width: 100%;
+    padding: 10px;
+    
+  }a:hover{
+    background-color: $orange;
+    transition: 0.2s;
+    color: $white;
+  }
+}
+
+</style>
---
+++ src/data/partials/culinary.js
@@ -0,0 +1,34 @@
+export default [
+  {
+    img: 'public/drinks-recipes.png',
+    text:'DRINK'
+  },
+  {
+    img: '/soups-recipes.png',
+    text:'SOUPS'
+  },
+  {
+    img: '/baking-recipes.png',
+    text:'BAKERY'
+  },
+  {
+    img: '/dinner-recipes.png',
+    text:'SOUP'
+  },
+  {
+    img: '/healthy-recipes.png',
+    text:'HEALTHY'
+  },
+  {
+    img: '/staff-picks.png',
+    text:'STAFF PICKS'
+  },
+  {
+    img: '/premium-recipes.png',
+    text:'APPETISERS'
+  },
+  {
+    img: 'public/quick-easy-recipes.png',
+    text:'QUICK & EASY'
+  },
+]
